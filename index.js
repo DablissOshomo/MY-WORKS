c3c311217593255a6c7f3a1d3b7bac608d2c3da0
DablissOshomo <daramolaayomide16@gmail.com>
2023-08-11 16:09:16 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ index.js
@@ -0,0 +1,36 @@
+const navbar = document.querySelector('nav');
+ window.onscroll = () => { 
+    if (window.scrollY > 100) {
+         navbar.classList.add('nav-active');
+         } else { 
+            navbar.classList.remove('nav-active'); } };
+
+
+
+
+
+ window.addEventListener('load', videoScroll);
+window.addEventListener('scroll', videoScroll);
+
+function videoScroll() {
+
+  if ( document.querySelectorAll('video[autoplay]').length > 0) {
+    var windowHeight = window.innerHeight,
+        videoEl = document.querySelectorAll('video[autoplay]');
+
+    for (var i = 0; i < videoEl.length; i++) {
+
+      var thisVideoEl = videoEl[i],
+          videoHeight = thisVideoEl.clientHeight,
+          videoClientRect = thisVideoEl.getBoundingClientRect().top;
+
+      if ( videoClientRect <= ( (windowHeight) - (videoHeight*.5) ) && videoClientRect >= ( 0 - ( videoHeight*.5 ) ) ) {
+        thisVideoEl.play();
+      } else {
+        thisVideoEl.pause();
+      }
+
+    }
+  }
+
+}
